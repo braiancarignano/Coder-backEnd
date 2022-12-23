e2d68d3e67d6728f8c4a14e540fa7a4bbaf5e701
Braian Carignano <braiancarignano@gmail.com>
2022-12-22 21:25:48 -0300

    add features (write/read/delete/upgrade)

--- index.js
+++ index.js
@@ -1,55 +1,102 @@
+const fs = require("fs");
+
+let product = {
+  title: "Smartphone",
+  description: "Este es un producto prueba",
+  price: 400,
+  thumbnail: "Sin Imagen",
+  code: "Smartphone123",
+  stock: 12,
+};
+
+let cocaCola = {
+  title: "Coca Cola",
+  description: "Bebida",
+  price: 300,
+  thumbnail: "Sin Imagen",
+  code: "Bebida123",
+  stock: 500,
+};
+let teclado = {
+  title: "Logitech",
+  description: "Teclado 100% USB",
+  price: 1500,
+  thumbnail: "Sin Imagen",
+  code: "Teclado123",
+  stock: 5,
+};
+
 class ProductManager {
-  products;
-  static id = 0;
-  constructor(title, description, price, thumbnail, code, stock) {
-    this.products = [];
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.thumbnail = thumbnail;
-    this.code = code;
-    this.stock = stock;
+  path;
+  constructor(path) {
+    this.path = path;
+    this.products = this.readFile();
   }
-  addProduct() {
-    let repeatProduct = this.products.find(
-      (product) => product.code === this.code
-    );
-    if (repeatProduct) {
-      return console.log("//////////------ ERROR: CODE ya asignado a otro producto. ------//////////");
+
+  readFile() {
+    const data = JSON.parse(fs.readFileSync(`./${this.path}`, "utf-8"));
+    return data;
+  }
+
+  writeData(data) {
+    let dataString = JSON.stringify(data);
+    fs.writeFileSync(`./${this.path}`, dataString);
+    return dataString;
+  }
+
+  addProduct(item) {
+    let items = this.readFile();
+    if (items.find((e) => e.code === item.code)) {
+      console.log("a CODE has already been assigned");
     } else {
-      let id = (ProductManager.id += 1);
-      this.products.push(product, id);
+      item.id = items.length > 0 ? items[items.length - 1].id + 1 : 1;
+      items.push(item);
+      this.writeData(items);
     }
   }
-  getProducts() {
-    return this.products;
-  }
   getProductById(id) {
-    let reserchId = this.products.find((product) => product.id === id);
-    return reserchId;
+    let idItem = this.readFile().find((e) => e.id === id);
+    if (idItem) {
+      return console.log(idItem);
+    }
+    console.log("ID not found");
+  }
+  updateProduct(id, product) {
+    let data = this.readFile();
+    if (data.find((product) => product.id === id)) {
+      let productDeleted = data.filter((product) => product.id !== id);
+      product.id = id;
+      productDeleted.push(product);
+      this.writeData(productDeleted);
+      return productDeleted;
+    } else {
+      console.log("The product to be updated does not exist");
+    }
+  }
+  deleteProduct(id) {
+    let products = this.readFile();
+    if(product = products.filter((e) => e.id != id)){
+    this.writeData(product);}
+    console.log("ID not found")
+  }
+  deleteAll() {
+    this.writeData([]);
   }
 }
-const product = new ProductManager(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin Imagen",
-  "abc123",
-  25
-);
-console.log(
-  "<========== mostrar array vacio ==========>",
-  product.getProducts()
-);
-//Agrega producto al array
-product.addProduct();
-console.log(
-  "<========== mostrar array con producto ==========>",
-  product.getProducts()
-);
-//Muestra el error al intentrar crear otro producto con mismo CODE
-product.addProduct();
-console.log(
-  "<========== mostrar si se encuentra el id ==========>",
-  product.getProductById()
-);
+const productManager = new ProductManager("products.json");
+
+// console.log(productManager.readFile());
+// productManager.addProduct(product);
+// productManager.addProduct(teclado);
+// productManager.addProduct(cocaCola);
+// productManager.getProductById(2);
+// productManager.updateProduct(1,{
+//   title: "Redragon",
+//   description: "Mouse Inalambrico 16000 Dpi",
+//   price: 1000,
+//   thumbnail: "Sin Imagen",
+//   code: "Mouse123",
+//   stock: 8,
+// })
+// productManager.deleteProduct(1)
+// productManager.deleteAll()
